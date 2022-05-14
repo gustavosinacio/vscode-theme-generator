168936ba3083bedeb8dfa03acdfc385e08623020
Gustavo Inacio <gsinacio94@gmail.com>
2022-05-14 17:06:55 -0300

    first commit

---
+++ colors.ts
@@ -0,0 +1,35 @@
+export const aqua300 = "#067";
+export const aqua400 = "#09a";
+export const aqua500 = "#00f0ff";
+export const aquaTransparent100 = "#0ff1";
+export const aquaTransparent300 = "#0ff3";
+export const aquaTransparent400 = "#0ff5";
+export const aquaTransparent500 = "#0ff7";
+
+export const purple = "#a852ff";
+export const purple2 = "#a5f";
+export const purpleLight = "#ffa0ff";
+export const neonPink = "#ff1f96";
+
+export const red = "#f33";
+export const redWarn = "#f44747";
+export const redError = "#ff7272";
+export const greenDark = "#085";
+export const green = "#0f5";
+export const greenLight = "#5fff7c";
+export const yellow = "#ffe65f";
+
+export const white = "#fff";
+export const black = "#000";
+
+export const gray100 = "#111";
+export const gray200 = "#222";
+export const gray300 = "#333";
+export const gray400 = "#555";
+export const gray500 = "#777";
+export const gray600 = "#999";
+export const gray700 = "#aaa";
+export const gray800 = "#ccc";
+export const gray900 = "#eee";
+
+export const transparent = "#0000";
---
+++ config.ts
@@ -0,0 +1,5 @@
+export default {
+  themePath: [
+    `${process.env.HOME}/github/vscode-theme-oled-neon/themes/oled-neon-dark.json`,
+  ],
+};
